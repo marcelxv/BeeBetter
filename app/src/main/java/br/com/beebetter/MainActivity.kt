0bfad7b6e530ed1d8f73c46d6e770ad297cc6e6a
Marcel Scognamiglio <67491786+marcelxv@users.noreply.github.com>
2022-08-20 19:59:14 -0300

    first commit

---
+++ app/src/main/java/br/com/beebetter/MainActivity.kt
@@ -0,0 +1,26 @@
+package br.com.beebetter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var buttonInitiate: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+        buttonInitiate = findViewById<Button>(R.id.button_iniciar)
+        Toast.makeText(this, "Seja bem-vindo!", Toast.LENGTH_SHORT).show()
+        buttonInitiate.setOnClickListener {
+            init()
+        }
+    }
+
+    private fun init() {
+    val initLogin = Intent(this, LoginActivity::class.java)
+        startActivity(initLogin)
+    }
+}
